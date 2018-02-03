296c3b7c6ad7c6d35c37679ece8dc15671561361
Julio Alorro <alorro3@gmail.com>
2018-02-04 01:33:32 +0800

    Stuff

--- Package.swift
+++ Package.swift
@@ -1,12 +1,16 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+//
+//  Astral
+//  Copyright (c) 2017-2018 Julio Miguel Alorro
+//  Licensed under the MIT license. See LICENSE file
+//
 
 import PackageDescription
 
 let package = Package(
     name: "Astral",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Astral",
             targets: ["Astral"]
@@ -16,8 +20,6 @@ let package = Package(
         .package(url: "https://github.com/Thomvis/BrightFutures", from: "6.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Astral",
             dependencies: ["BrightFutures"],
